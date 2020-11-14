d81eb0324826bcf8d9f62c5fd7cb397b9ae0b6da
CleoMugs <cleophas.mugeni@gmail.com>
2020-11-14 20:47:41 +0300

    runtime & requirements

---
+++ src/samples/keys.py
@@ -0,0 +1,8 @@
+business_shortCode = "174379" # Lipa Na Mpesa Shortcode
+phone_number = "254708252968" # Person sending phone number
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" #
+consumer_key = "vP8FmQ5lHOkA8lWSsVlYMl2G8nhuas56" #
+consumer_secret = "SmAUhoUbBmsDySfp" #
+
+shortcode = "601431"
+test_msisdn = "254708374149"
